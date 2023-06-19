014f0bf2b0ab0b54f11c8f19e6eea31f0f43fdf9
dhanushka jayathilaka <djayathilaka@kaleris.com>
2023-06-19 20:21:08 +0530

    this is first commit for freature branch

--- src/com/kaleris/net/ClassA.java
+++ src/com/kaleris/net/ClassA.java
@@ -3,7 +3,7 @@ package com.kaleris.net;
 
 public class ClassA {
 	public static void main(String[] args) {
-		System.out.println("Dhanushka");
+		System.out.println("Dhanushka this 1st commit for feature 1");
 	}
 
 }
