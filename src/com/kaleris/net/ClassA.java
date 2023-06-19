0b8cf9cb55fc226784f1d89049320005f08195b9
dhanushka jayathilaka <djayathilaka@kaleris.com>
2023-06-19 20:26:30 +0530

    this is the second commit for freature branch

--- src/com/kaleris/net/ClassA.java
+++ src/com/kaleris/net/ClassA.java
@@ -4,6 +4,8 @@ package com.kaleris.net;
 public class ClassA {
 	public static void main(String[] args) {
 		System.out.println("Dhanushka");
+		
+		System.out.println("this is new commit 1 for main branch");
 	}
 
 }
